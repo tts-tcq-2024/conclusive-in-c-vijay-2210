84b19e128301ada2510388c220fdcfb7412c0368
vijay-2210 <73438496+vijay-2210@users.noreply.github.com>
2024-08-27 09:06:46 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,46 +1,9 @@
 #include <gtest/gtest.h>
-#include "breach_detector.h"
-#include "alert_to_controller.h"
 #include "AlertInEmail.h"
-#include "cooling_config.h"
 #include "TemperatureAlert_checker.h"
-
-TEST(InferBreachTest, TemperatureTooLow) {
-  EXPECT_EQ(infer_breach(-1, 0, 35), TOO_LOW);
-  EXPECT_EQ(infer_breach(9, 10, 35), TOO_LOW);
-}
-
-TEST(InferBreachTest, TemperatureTooHigh) {
-  EXPECT_EQ(infer_breach(40, 0, 39), TOO_HIGH);
-  EXPECT_EQ(infer_breach(65, 0, 60), TOO_HIGH);
-}
-
-TEST(InferBreachTest, TemperatureNormal) {
-  EXPECT_EQ(infer_breach(5, 0, 35), NORMAL);
-  EXPECT_EQ(infer_breach(30, 0, 40), NORMAL);
-  EXPECT_EQ(infer_breach(40, 0, 50), NORMAL);
-}
-
-TEST(ClassifyTemperatureBreachTest, PassiveCooling) {
-  EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, 0), NORMAL);
-  EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, 35), NORMAL);
-  EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, -1), TOO_LOW);
-  EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, 36), TOO_HIGH);
-}
-
-TEST(ClassifyTemperatureBreachTest, HighActiveCooling) {
-  EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, 0), NORMAL);
-  EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, 45), NORMAL);
-  EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, -1), TOO_LOW);
-  EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, 46), TOO_HIGH);
-}
-
-TEST(ClassifyTemperatureBreachTest, MedActiveCooling) {
-  EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, 0), NORMAL);
-  EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, 40), NORMAL);
-  EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, -1), TOO_LOW);
-  EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, 41), TOO_HIGH);
-}
+#include "alert_to_controller.h"
+#include "breach_detector.h"
+#include "cooling_config.h"
 
 TEST(CheckAndAlertTest, SendToController) {
   battery_character batteryChar = {PASSIVE_COOLING, "Battery 1"};
@@ -55,3 +18,42 @@ TEST(CheckAndAlertTest, SendToEmail) {
   check_and_alert(TO_EMAIL, batteryChar, 36);
   check_and_alert(TO_EMAIL, batteryChar, 25);
 }
+
+TEST(InferBreachTest, TemperatureTooLow) {
+  EXPECT_EQ(infer_breach(9, 10, 35), TOO_LOW);
+  EXPECT_EQ(infer_breach(-1, 0, 35), TOO_LOW);  
+}
+
+TEST(InferBreachTest, TemperatureTooHigh) {
+  EXPECT_EQ(infer_breach(65, 0, 60), TOO_HIGH);
+  EXPECT_EQ(infer_breach(40, 0, 39), TOO_HIGH);
+}
+
+TEST(InferBreachTest, TemperatureNormal) { 
+  EXPECT_EQ(infer_breach(30, 0, 40), NORMAL);
+  EXPECT_EQ(infer_breach(5, 0, 35), NORMAL);
+  EXPECT_EQ(infer_breach(40, 0, 50), NORMAL);
+}
+
+TEST(ClassifyTemperatureBreachTest, PassiveCooling) {
+  EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, -1), TOO_LOW);
+  EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, 0), NORMAL);
+  EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, 35), NORMAL);
+  EXPECT_EQ(classify_temperature_breach(PASSIVE_COOLING, 36), TOO_HIGH);
+}
+
+TEST(ClassifyTemperatureBreachTest, HighActiveCooling) {
+  EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, -1), TOO_LOW);
+  EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, 0), NORMAL);
+  EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, 45), NORMAL);
+  EXPECT_EQ(classify_temperature_breach(HI_ACTIVE_COOLING, 46), TOO_HIGH);
+}
+
+TEST(ClassifyTemperatureBreachTest, MedActiveCooling) {
+  EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, -1), TOO_LOW);
+  EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, 0), NORMAL);
+  EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, 40), NORMAL);
+  EXPECT_EQ(classify_temperature_breach(MED_ACTIVE_COOLING, 41), TOO_HIGH);
+}
+
+
